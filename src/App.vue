290dda3d783d391a20e168f91d1e09c13bd83b61
unknown <407249117@qq.com>
2021-07-22 19:04:35 +0800

    cli-upgrade

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
   <div id="app">
-<!--    除了Detail之外其他页面都可以被缓存-->
+    <!--    除了Detail之外其他页面都可以被缓存-->
     <keep-alive exclude="Detail">
       <router-view/>
     </keep-alive>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
-</style>
